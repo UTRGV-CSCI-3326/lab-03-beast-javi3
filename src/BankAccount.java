6c79d248d48d509cfd18dac492f439807c3f4a90
beast-javi3 <javiala0104@gmail.com>
2025-02-01 22:17:33 +0000

    third commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,27 @@
+public class BankAccount{
+public static void main(String[] args){
+float balance = 2175.37f; 
+//System.out.println(" Bob has a total balance of "  + balance );
 
+balance -= 302.50f;
+
+System.out.println(String.format( "%.2f" , balance) );
+
+balance += 50.03f;
+System.out.println(String.format( "%.2f" ,balance));
+
+balance /=2;
+balance += 20;
+System.out.println(String.format( "%.2f",balance));
+
+balance -=1;
+System.out.println(String.format("%.2f" , balance));
+
+balance *=2;
+System.out.println(String.format("%.2f" , balance));
+
+balance +=1;
+System.out.println(String.format("%.2f", balance ));
+System.out.println( "BOb's new balance after all of his transactions " + String.format("%.2f ", balance));
+}
+}
